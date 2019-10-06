2f830355b4b266c233411151ba10300000583d49
popa dorin <dorinmarian89@gmail.com>
2019-10-06 15:45:02 +0300

    added overloading

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,10 +4,23 @@ package org.fasttrackit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.addition(3, 23);
+        calculator.decreases(5, 7);
+        calculator.multiplication(10, 35);
+
+        calculator.addition(2.5, 6.166);
+        calculator.decrease(6.67, 77.88);
+        calculator.multiplication(6.75, 1.799);
+
+        calculator.addition(12.32, 7);
+        calculator.decrease(3.68, 1);
+        calculator.multiplication(6.8, 7);
+
+        calculator.addition(112, 45.76);
+        calculator.decrease(67, 34.345);
+        calculator.multiplication(10, 14.77);
     }
 }
---
+++ src/main/java/org/fasttrackit/Calculator.java
@@ -0,0 +1,80 @@
+package org.fasttrackit;
+
+public class Calculator{
+    public double addition(double num1, int num2) {
+        double addition = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + addition);
+        return addition;
+    }
+
+    public double decrease(double num1, int num2) {
+        double decreases = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + decreases);
+
+        return decreases;
+    }
+
+    public double multiplication(double num1, int num2) {
+        double multiplication = num1 * num2;
+        System.out.println(num1 + "*" + num2 + "=" + multiplication);
+        return multiplication;
+    }
+    public double addition(int num1, double num2) {
+        double addition = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + addition);
+        return addition;
+    }
+
+    public double decrease(int num1, double num2) {
+        double decreases = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + decreases);
+
+        return decreases;
+    }
+
+    public double multiplication(int num1, double num2) {
+        double multiplication = num1 * num2;
+        System.out.println(num1 + "*" + num2 + "=" + multiplication);
+        return multiplication;
+    }
+
+
+
+    public double addition(double num1, double num2) {
+        double addition = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + addition);
+        return addition;
+    }
+
+    public double decrease(double num1, double num2) {
+        double decreases = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + decreases);
+
+        return decreases;
+    }
+
+    public double multiplication(double num1, double num2) {
+        double multiplication = num1 * num2;
+        System.out.println(num1 + "*" + num2 + "=" + multiplication);
+        return multiplication;
+    }
+
+    public int addition(int num1, int num2) {
+        int addition = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + addition);
+        return addition;
+    }
+
+    public int decreases(int num1, int num2) {
+        int decreases = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + decreases);
+        return decreases;
+    }
+
+    public int multiplication(int num1, int num2) {
+        int multiplication = num1 * num2;
+        System.out.println(num1 + "*" + num2 + "=" + multiplication);
+        return multiplication;
+    }
+}
+
